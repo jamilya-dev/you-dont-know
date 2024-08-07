8dec4a9f1566cb501f442db54fc556c5b7972fa3
jamilya-dev <omg.groups@yandex.ru>
2024-08-07 09:35:26 +0300

    доработка задагия 10

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const marketing = document.querySelector('.adv');
 const list2 = books[0].querySelectorAll('li');
 const list5 = books[5].querySelectorAll('li');
 const list6 = books[2].querySelectorAll('li');
-const item8 = document.createElement('li')
+const item8 = document.createElement('li');
+const book4Title = books[4].querySelector('a');
 
 container.prepend(books[1]);
 books[0].after(books[4]);
@@ -15,6 +16,8 @@ books[5].before(books[2]);
 container.append(books[2]);
 
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+book4Title.innerHTML = "Книга 3. this и <b>Прототипы</b> Объектов"
 
 marketing.remove();
 
@@ -28,5 +31,3 @@ list5[8].before(list5[5]);
 
 item8.textContent = "Глава 8: За пределами ES6";
 list6[8].after(item8);
-
-
